ba8dea08837520493a6aa0f9a3ac513fe725f662
ponsea <puodsea@gmail.com>
2017-08-12 15:30:00 +0100

    Add "section" field to seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,7 @@ CSV.foreach('db/phrases.txt', col_sep: "\t") do |row|
     answer: row[3],
     meanings: row[4],
     explanation: row[5],
-    level: row[6].to_i
+    section: row[6].to_i,
+    level: row[7].to_i
   )
 end
