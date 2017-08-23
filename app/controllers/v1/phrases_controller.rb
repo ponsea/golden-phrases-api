14b24ed5549cefddf20ea1473b01131ac5607a18
ponsea <puodsea@gmail.com>
2017-08-23 09:16:05 +0100

    Make phrases selectable from section parameter

--- app/controllers/v1/phrases_controller.rb
+++ app/controllers/v1/phrases_controller.rb
@@ -1,8 +1,11 @@
 class V1::PhrasesController < ApplicationController
 
   def index
-    @phrases = Phrase.all
-
-    render json: @phrases
+    if section = params['section']
+      @phrases = Phrase.where(section: section)
+      render json: @phrases
+    else
+      render json: {message: 'section parameter is required'}, status: 400
+    end
   end
 end
