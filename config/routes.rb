e3444c8502590e048ac86f697720a9e99aab9694
ponsea <puodsea@gmail.com>
2017-09-03 06:11:16 +0100

    Add User model on devise token auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :v1 do
+    mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
     resources :phrases, only: :index
   end
 end
