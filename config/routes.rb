174818ecc000a0698345bdc814d0f8acc2c75b65
ponsea <puodsea@gmail.com>
2017-09-12 02:32:53 +0100

    Add RegistrationsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :v1 do
-    mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
+    mount_devise_token_auth_for 'User', at: 'auth',
+                                skip: [:omniauth_callbacks],
+                                controllers: { registrations: 'v1/auth/registrations' }
     resources :phrases, only: :index
     resources :levels, only: :index
   end
