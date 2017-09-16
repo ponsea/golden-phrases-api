1876cbf9311fb705deafa012c3ddfa72bbc116e3
ponsea <puodsea@gmail.com>
2017-09-16 12:03:48 +0000

    Add a filter for resolving a problem of devise_token_auth (gem)
    See https://github.com/lynndylanhurley/devise_token_auth/issues/702

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :set_response_headers
+
+  def set_response_headers
+    # TODO: Remove this once the following issue is fixed in DTA
+    # https://github.com/lynndylanhurley/devise_token_auth/issues/702
+    # response.headers["Cache-Control"] = "no-cache, no-store"
+    if v1_user_signed_in? and response.headers[DeviseTokenAuth.headers_names[:"access-token"]].nil?
+      auth_header = {}
+      auth_header[DeviseTokenAuth.headers_names[:"access-token"]] = ''
+      auth_header[DeviseTokenAuth.headers_names[:"client"]] = ''
+      auth_header[DeviseTokenAuth.headers_names[:"expiry"]] = ''
+      response.headers.merge!(auth_header)
+    end
+  end
 end
