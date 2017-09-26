baff754d0cc98b4a856c0f830c030f79302ceea0
ponsea <puodsea@gmail.com>
2017-09-26 04:06:52 +0000

    Make it possible to confirm a e-mail address

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,4 +44,17 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => 'smtp.gmail.com',
+    :port => '587',
+    :domain => 'smtp.gmail.com',
+    :authentication => 'plain',
+    :user_name => ENV['GP_EMAIL'],
+    :password => ENV['GP_EMAIL_PASS']
+  }
 end
---
+++ config/initializers/devise.rb
@@ -0,0 +1,3 @@
+Devise.setup do |config|
+  config.mailer_sender = "金のフレーズ <#{ENV['GP_EMAIL']}>"
+end
